c1b826e080cec6c97e036a6cd7344ddf65f6ffe7
Anton <antonpolyakovvv@mail.ru>
2021-03-10 02:39:01 +0300

    Upload project

---
+++ data.js
@@ -0,0 +1,30 @@
+let data = [
+    {
+        name: "morning",
+        temp: 0,
+        precipitation: 5,
+        icon: "img/morning.png",
+        colors: ["#FF69B4", "#FFA07A"]
+    },
+    {
+        name: "day",
+        temp: 5,
+        precipitation: 12,
+        icon: "img/day.png",
+        colors: ["#FFD700", "#FF4500"]
+    },
+    {
+        name: "evening",
+        temp: 3,
+        precipitation: 82,
+        icon: "img/evening.png",
+        colors: ["#DA70D6", "#4B0082"]
+    },
+    {
+        name: "night",
+        temp: -2,
+        precipitation: 90,
+        icon: "img/night.png",
+        colors: ["#87CEFA", "#00008B"]
+    }
+]
